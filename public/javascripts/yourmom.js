264d348c41e771a495c8f92528feb1004558a323
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2023-01-27 15:40:26 -0700

    Update POC to take sensitive values at runtime

--- public/javascripts/yourmom.js
+++ public/javascripts/yourmom.js
@@ -1,31 +1,60 @@
-const appId = "";
-const locationId = "";
+let appId;
+let locId;
 
 document.addEventListener("DOMContentLoaded", async function () {
     if (!window.Square) {
         throw new Error("Square.js failed to load properly");
     }
 
+    const cardButton = document.getElementById("card-button");
+    const varButton = document.getElementById("var-button");
+    const form = document.getElementById("input-form");
+
+    varButton.addEventListener("click", async function (event) {
+        const applicationId = document.getElementById("appId");
+        const locationId = document.getElementById("locId");
+
+        if (applicationId.value == "" || locationId.value == "") {
+            const status = document.getElementById("status");
+            status.innerHTML = `<p>(This won't work unless you fill in the application and location ID...)</p>`;
+            return;
+        }
+
+        appId = applicationId.value;
+        locId = locationId.value;
+
+        varButton.style.display = "none";
+        form.style.display = "none";
+
+        await goTokenize();
+    });
+});
+
+async function goTokenize() {
     let payments;
     try {
-        payments = window.Square.payments(appId, locationId);
+        payments = window.Square.payments(appId, locId);
     } catch {
-        alert("Error");
+        const status = document.getElementById("status");
+        status.innerHTML = `<p>(That didn't work. Check your appId and locId values.)</p>`;
+        return;
     }
+
+    const cardButton = document.getElementById("card-button");
+    cardButton.style.display = "block";
 
     const card = await payments.card();
     await card.attach("#card-container");
 
-    const cardButton = document.getElementById("card-button");
     cardButton.addEventListener("click", async function (event) {
-        cardButton.disabled = true;
-        const nonce = document.getElementById("nonce-token");
+        cardButton.style.display = "none";
+        const status = document.getElementById("status");
         let tokenResult;
         try {
             tokenResult = await card.tokenize();
-            nonce.innerHTML = `<p>Nonce/token: <span style="color:green">${tokenResult.token}</span></p>`;
+            status.innerHTML = `<p>Nonce/token: <span style="color:green">${tokenResult.token}</span></p>`;
         } catch (err) {
-            nonce.innerHTML = `<p>Error: <span style="color:red">${err.message}<span></p>`;
+            status.innerHTML = `<p>Error: <span style="color:red">${err.message}<span></p>`;
         }
     });
-});
+}
